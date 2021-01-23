0161e57ad1071a692782c51bea095d52ffcfb8e2
Victor Socaciu <socaciu.victor@gmail.com>
2021-01-23 21:50:38 +0200

    added option to close store

--- src/components/MarketComponents/MarketData.js
+++ src/components/MarketComponents/MarketData.js
@@ -1,10 +1,26 @@
-import { Button, Grid, TextField } from '@material-ui/core'
+import { Button, Grid, makeStyles, TextField, Typography } from '@material-ui/core'
 import { useSnackbar } from 'notistack'
 import React, { useEffect, useState } from 'react'
 import { isEmpty, isLoaded, useFirestore } from 'react-redux-firebase'
 import { useMarketContext } from './MarketContext/MarketContext'
 
+const useStyles = makeStyles((theme) => ({
+    deactivateButton: {
+        // margin: theme.spacing(3, 0, 2),
+        color: theme.palette.error.contrastText,
+        backgroundColor: theme.palette.error.main,
+        "&:hover": {
+            backgroundColor: theme.palette.error.dark,
+            "@media (hover: none)": {
+                backgroundColor: theme.palette.error.main
+            }
+        }
+    },
+}))
+
 export default function MarketData() {
+
+    const classes = useStyles()
     const marketContext = useMarketContext()
     const [marketData, setMarketData] = useState({})
     const [isLoading, setIsLoading] = useState(false)
@@ -24,6 +40,19 @@ export default function MarketData() {
             setIsLoading(true)
             await firestore.collection('stores').doc('CosmoMarket').update(marketData)
         } catch (err) {
+            console.log(err)
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    const enableStore = async (enable) => {
+        try {
+            setIsLoading(true)
+            await firestore.collection('stores').doc('CosmoMarket').update({ isDisabled: !enable, isOpen: enable })
+            enqueueSnackbar('S-au actualizat datele', { variant: 'success' })
+        } catch (err) {
+            enqueueSnackbar('Datele nu au putut fi actualizate', { variant: 'error' })
             console.log(err)
         } finally {
             setIsLoading(false)
@@ -84,37 +113,134 @@ export default function MarketData() {
                         disabled={isLoading}
                     />
                 </Grid>
+                <Grid item>
+                    <Typography>
+                        Orar în timpul săptămânii (L - V)
+                    </Typography>
+                </Grid>
                 <Grid container item spacing={2}>
                     <Grid item xs={12} sm={6}>
                         <TextField
                             fullWidth
                             variant='outlined'
-                            label='Orar în timpul săptămânii'
-                            value={marketData.weekTimeTable || ''}
-                            onChange={(e) => setMarketData({ ...marketData, weekTimeTable: e.target.value })}
+                            label='Oră deschidere magazin'
+                            value={marketData.openHour || ''}
+                            onChange={(e) => {
+                                if (isNaN(e.target.value)) {
+                                    enqueueSnackbar('Ora este incorectă', { variant: 'error' })
+                                    return
+                                } else {
+                                    const hour = parseInt(e.target.value)
+                                    if (hour < 0 || hour > 23)
+                                        enqueueSnackbar('Ora este incorectă', { variant: 'error' })
+                                    else
+                                        setMarketData({ ...marketData, openHour: hour || 0 })
+                                }
+                            }}
                             disabled={isLoading}
                         />
                     </Grid>
-                    <Grid item xs={12} sm={6} >
+                    <Grid item xs={12} sm={6}>
                         <TextField
                             fullWidth
                             variant='outlined'
-                            label='Orar în weekend'
-                            value={marketData.weekendTimeTable || ''}
-                            onChange={(e) => setMarketData({ ...marketData, weekendTimeTable: e.target.value })}
+                            label='Oră închidere magazin'
+                            value={marketData.closeHour || ''}
+                            onChange={(e) => {
+                                if (isNaN(e.target.value)) {
+                                    enqueueSnackbar('Ora este incorectă', { variant: 'error' })
+                                    return
+                                } else {
+                                    const hour = parseInt(e.target.value)
+                                    if (hour < 0 || hour > 23)
+                                        enqueueSnackbar('Ora este incorectă', { variant: 'error' })
+                                    else
+                                        setMarketData({ ...marketData, closeHour: hour || 0 })
+                                }
+                            }}
                             disabled={isLoading}
                         />
                     </Grid>
+                    <Grid item>
+                        <Typography>
+                            Orar în weekend (S - D)
+                        </Typography>
+                    </Grid>
+                    <Grid container item spacing={2}>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                fullWidth
+                                variant='outlined'
+                                label='Oră deschidere magazin'
+                                value={marketData.weekendOpenHour || ''}
+                                onChange={(e) => {
+                                    if (isNaN(e.target.value)) {
+                                        enqueueSnackbar('Ora este incorectă', { variant: 'error' })
+                                        return
+                                    } else {
+                                        const hour = parseInt(e.target.value)
+                                        if (hour < 0 || hour > 23)
+                                            enqueueSnackbar('Ora este incorectă', { variant: 'error' })
+                                        else
+                                            setMarketData({ ...marketData, weekendOpenHour: hour || 0 })
+                                    }
+                                }}
+                                disabled={isLoading}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                fullWidth
+                                variant='outlined'
+                                label='Oră închidere magazin'
+                                value={marketData.weekendCloseHour || ''}
+                                onChange={(e) => {
+                                    if (isNaN(e.target.value)) {
+                                        enqueueSnackbar('Ora este incorectă', { variant: 'error' })
+                                        return
+                                    } else {
+                                        const hour = parseInt(e.target.value)
+                                        if (hour < 0 || hour > 23)
+                                            enqueueSnackbar('Ora este incorectă', { variant: 'error' })
+                                        else
+                                            setMarketData({ ...marketData, weekendCloseHour: hour || 0 })
+                                    }
+                                }}
+                                disabled={isLoading}
+                            />
+                        </Grid>
+                    </Grid>
                 </Grid>
-                <Grid item>
-                    <Button
-                        variant='contained'
-                        color='primary'
-                        type='submit'
-                        disabled={isLoading}
-                    >
-                        Actualizează
-                        </Button>
+                <Grid container item justify='space-between'>
+                    <Grid item>
+                        <Button
+                            variant='contained'
+                            color='primary'
+                            type='submit'
+                            disabled={isLoading}
+                        >
+                            Actualizează
+                    </Button>
+                    </Grid>
+                    <Grid item>
+                        {!marketData.isDisabled ? <Button
+                            variant='contained'
+                            className={classes.deactivateButton}
+                            disabled={isLoading}
+                            onClick={() => enableStore(false)}
+                        >
+                            Dezactivează magazinul
+                            </Button>
+                            :
+                            <Button
+                                variant='contained'
+                                color='primary'
+                                disabled={isLoading}
+                                onClick={() => enableStore(true)}
+                            >
+                                Reactivează magazinul
+                    </Button>}
+                    </Grid>
                 </Grid>
             </Grid>
         </form>
